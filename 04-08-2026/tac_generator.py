d5d386ab06b78a2a543ced64a645eb7085bb4745
Kompalli Harika <kompalliharika6@gmail.com>
2026-08-18 09:33:22 +0530

    started

--- 04-08-2026/tac_generator.py
+++ 04-08-2026/tac_generator.py
@@ -14,7 +14,7 @@ from three_address_code import TripleTAC, BinOpTriple, AssignTriple, PrintTriple
 
 class TACGenerator:
     def __init__(self):
-        self.program = TripleProgram()
+        self.program = TripleTAC()
 
     def generate(self, function):
         """
@@ -40,7 +40,13 @@ class TACGenerator:
               self.program.append(PrintTriple(operand))
 
         """
-        raise NotImplementedError("implement TACGenerator.gen_stmt()")
+        #raise NotImplementedError("implement TACGenerator.gen_stmt()")
+        if isinstance(stmt, Assign):
+            operand = self.gen_expr(stmt.expr)
+            self.program.append(AssignTriple(stmt.var.name, operand))
+        elif isinstance(stmt, Print):
+              operand = self.gen_expr(stmt.expr)
+              self.program.append(PrintTriple(operand))
 
     def gen_expr(self, node):
         """
@@ -67,6 +73,16 @@ class TACGenerator:
         this node's own triple -- otherwise triples come out numbered in
         the wrong order and later TripleRefs point at the wrong thing.
         """
-        raise NotImplementedError("implement TACGenerator.gen_expr()")
+        #raise NotImplementedError("implement TACGenerator.gen_expr()")        
+        if isinstance(node, Var):
+            return node.name
+        elif isinstance(node, Num):
+            return node.value
+        elif isinstance(node, BinOp):
+            left  = self.gen_expr(node.left)
+            right = self.gen_expr(node.right)
+            return self.program.append(BinOpTriple(node.op, left, right)) 
+        
+
 
 
--- 18-07-2026/CalcLexer.py
+++ 18-07-2026/CalcLexer.py
@@ -6,13 +6,13 @@ class CalcLexer(Lexer):
     NUMBER = r'[0-9]+'
 
     def NUMBER(self, t):
-        #print(' token is', t.type, t.value)
+        print(' token is', t.type, t.value)
         t.value = int(t.value)
         return t
 
 #to test run Lexer independently following code used
-#inp = '2+3*5'
-#lexer = CalcLexer()
+inp = '2+3*5'
+lexer = CalcLexer()
 # tokenize function which takes input string and returns tokens
-#for token in lexer.tokenize(inp):
-#    print( token.type, token.value,type(token.value))
+for token in lexer.tokenize(inp):
+    print( token.type, token.value,type(token.value))
